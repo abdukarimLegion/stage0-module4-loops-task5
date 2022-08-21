b7fbf5a268b31ff4819c228c05e771b7cda5c9f9
Abdukarim <abdukarimuz@mail.ru>
2022-08-21 22:32:53 +0500

    done1and2

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -1,18 +1,16 @@
 package school.mjc.stage0.loops.task5;
 
 public class Cross {
-    public void printCross(int n) {
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++){
-                if( i==j || i+j==n-1){
-                    System.out.print("8");
-
-                }else{
-                    System.out.print(" ");
+    public void printCross(int sideLength) {
+        if (sideLength>0){
+            for (int i = 0; i < sideLength; i++) {
+                for (int j = 0; j < sideLength; j++) {
+                    if (i==sideLength/2||j==sideLength/2)
+                        System.out.print(8);
+                    else System.out.print(" ");
                 }
-            }
-            System.out.println();
-        }
+                System.out.println();//
+            }}
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,31 +2,12 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-        int i, j, k;
-
-        for (i = height; i >0; i--) {
-            for (j = height-i; j >=1; j--){
-                System.out.print(" ");
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < height; j++) {
+                if ((i <= j && (i + j) < height) || (i >= j && (i + j) >= height - 1))
+                    System.out.print(8);
+                else System.out.print(" ");
             }
-
-            for (k = 1; k <= i; k++){
-                System.out.print("8 ");
-            }
-
-
-            System.out.println();
-        }
-
-        for (i = 1; i <=height; i++) {
-
-            for (j = 1; j <=height-i; j++){
-                System.out.print(" ");
-            }
-            for (k = 1; k <= i; k++){
-                System.out.print("8 ");
-            }
-
-
             System.out.println();
         }
     }
